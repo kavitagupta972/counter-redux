62f37e75b25afed45a949eaab49d90959cdc94fd
Kavita Gupta <kavitagupta972@gmail.com>
2021-02-25 15:46:47 +0530

    Adding redux implementation with counter app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { useSelector, useDispatch  } from "react-redux";
+import { increment, decrement } from "./actions";
+function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+  return (
+    <div className="App">
+      <h1>Counter {counter}</h1>
+      <button onClick = {() => dispatch(increment(1))}> +</button>
+      <button onClick = {() => dispatch(decrement())}>-</button>
+      {isLogged ? <h3>Valuable Info</h3> : ""}
+      
+
+    </div>
+  );
+}
+
+export default App;
